e37eb50d59acfda3d4f1e57e2e7f291733dd145a
vargalott <156085832+vargalott@users.noreply.github.com>
2022-11-03 16:06:02 +0300

    add(fix) lint workflow

---
+++ .github/workflows/lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+is_bash() {
+    [[ $1 == *.sh ]] && return 0
+    [[ $1 == */bash-completion/* ]] && return 0
+    [[ $(file -b --mime-type "$1") == text/x-shellscript ]] && return 0
+    return 1
+}
+
+while IFS= read -r -d $'' file; do
+    if is_bash "$file"; then
+        shellcheck -e SC1090 -W0 -s bash "$file"
+    fi
+done < <(find . -type f \! -path "./.git/*" -print0)
